7fb64c09091f7b536a2eaf3f9452590b1edede6d
yanlingao <445465143@qq.com>
2018-09-27 10:18:13 +0800

    修正判断一个元素的耗时结果的性能测试。

--- src/test/java/com/ayl/bloomfilter/RedisBloomFilterTest.java
+++ src/test/java/com/ayl/bloomfilter/RedisBloomFilterTest.java
@@ -29,29 +29,39 @@ public class RedisBloomFilterTest {
 
     /**
      *  //得到的结果是：
-     *  // 判断一个总共耗时：3406ms
-     *  // 总共耗时：3410ms
+     *  // add耗时：3561ms
+     *  // 判断一个总共耗时：2ms
+     *  // 总共耗时：7ms
      *  // 首先，这个响应时间并不怎么理想
      *  // 其次，看上去判断一个和判断多个的时间几乎没有区别。很可能是因为redis是部署在本地，没有tcp延迟的缘故，如果是远程机器，应该差别会很明显
      */
     @Test
     public void filterTest() {
         long startTime = new Date().getTime();
+        //添加insertions个目标元素到过滤器中
         String prefix = "element";
         for (int i = 0; i< insertions; i++) {
             filter.add(prefix + i);
         }
+        //查看添加过程耗时
+        long entTime2 = new Date().getTime();
+        System.out.println("add耗时：" + (entTime2 - startTime) + "ms");
+
+        long startTime1 = new Date().getTime();
         Assert.assertTrue(filter.contains(prefix + 0));
+        //查看判断一个元素的耗时
         long entTime1 = new Date().getTime();
-        System.out.println("判断一个总共耗时：" + (entTime1 - startTime) + "ms");
+        System.out.println("判断一个总共耗时：" + (entTime1 - startTime1) + "ms");
+
         Assert.assertTrue(filter.contains(prefix + (insertions -1) ));
         Assert.assertTrue(filter.contains(prefix + 888));
 
         Assert.assertFalse(filter.contains(prefix + -1));
-        Assert.assertFalse(filter.contains(prefix + 100000));
-        Assert.assertFalse(filter.contains(prefix + 10001));
+        Assert.assertFalse(filter.contains(prefix + insertions));
+        Assert.assertFalse(filter.contains(prefix + (insertions + 1)));
+        //查看判断多个元素的耗时
         long entTime = new Date().getTime();
-        System.out.println("总共耗时：" + (entTime - startTime) + "ms");
+        System.out.println("总共耗时：" + (entTime - startTime1) + "ms");
     }
 
 }
